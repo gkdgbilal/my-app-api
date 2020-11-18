34c81296e0927a3a1fb8b708ef1bad1025725010
gkdgbilal <bilal.gkdg@gmail.com>
2020-11-18 15:09:09 +0300

    added updated date

--- main.go
+++ main.go
@@ -126,7 +126,6 @@ type User struct {
 	Password    string             `bson:"password" json:"password"`
 	CreatedDate time.Time          `bson:"createdDate" json:"createdDate"`
 	UpdatedDate time.Time          `bson:"updatedDate" json:"updatedDate"`
-	DeletedDate time.Time          `bson:"deletedDate" json:"deletedDate"`
 }
 
 func Connect() (*mongo.Database, error) {
